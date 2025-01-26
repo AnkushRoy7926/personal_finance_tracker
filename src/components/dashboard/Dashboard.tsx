dadd3aeabd8aa3297e8e4c9e28ebd2856460b542
LordRaleigh7926 <ankushroy7926@gmail.com>
2025-01-26 18:23:42 +0530

    fixed theme error

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -37,12 +37,11 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
           component="main"
           sx={(theme) => ({
             flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
+            backgroundColor: alpha(theme.palette.background.default, 1),
             overflow: 'auto',
           })}
         >
+          
           <Stack
             spacing={2}
             sx={{
